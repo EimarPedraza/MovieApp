c72403c7c07e2bddc73d22f3a7e98001408a38ff
Eimar Pedraza <eimar2alveiro@gmail.com>
2025-04-19 00:11:33 -0500

    IMPLEMENTACION - Se finaliza el DataSource
    - Se agrega llamado al webservice desde la clase MovieDataSource
    - Se ajustan los métodos del MovieDataSource para que sean de tipo suspend debido a que se llaman a los métodos de webservice.
    - Se agrega constante de lenguage español-mexico

--- app/src/main/java/com/eapvlab/movieapp/application/AppConstants.kt
+++ app/src/main/java/com/eapvlab/movieapp/application/AppConstants.kt
@@ -5,5 +5,6 @@ object AppConstants {
     //API
     const val BASE_URL = "https://api.themovie.org/3/"
     const val API_KEY = "AyElDrilococoElCreeQueNoLoVi"
+    const val LAN_ES_MX = "es-MX"
 
 }
--- app/src/main/java/com/eapvlab/movieapp/data/remote/MovieDataSource.kt
+++ app/src/main/java/com/eapvlab/movieapp/data/remote/MovieDataSource.kt
@@ -1,19 +1,21 @@
 package com.eapvlab.movieapp.data.remote
 
+import com.eapvlab.movieapp.application.AppConstants
 import com.eapvlab.movieapp.data.model.MovieList
+import com.eapvlab.movieapp.repository.WebService
 
-class MovieDataSource {
+class MovieDataSource(private val webService: WebService) {
 
-    fun getUpcomingMovies(): MovieList {
-        return MovieList()
+    suspend fun getUpcomingMovies(): MovieList {
+        return webService.getUpComingMovies(AppConstants.API_KEY, AppConstants.LAN_ES_MX)
     }
 
-    fun getTopRatedMovies(): MovieList {
-        return MovieList()
+    suspend fun getTopRatedMovies(): MovieList {
+        return webService.getTopRatedMovies(AppConstants.API_KEY, AppConstants.LAN_ES_MX)
     }
 
-    fun getPopularMovies(): MovieList {
-        return MovieList()
+    suspend fun getPopularMovies(): MovieList {
+        return webService.getPopularMovies(AppConstants.API_KEY, AppConstants.LAN_ES_MX)
     }
 
 }
