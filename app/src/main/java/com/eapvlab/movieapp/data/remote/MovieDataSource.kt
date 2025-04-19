60eda69e42bec29e8c743cc10dfd5e350682a357
Eimar Pedraza <eimar2alveiro@gmail.com>
2025-04-18 23:20:09 -0500

    IMPLEMENTACION - Se agregan las funciones base para obtener los litados de Movies

--- app/src/main/java/com/eapvlab/movieapp/data/remote/MovieDataSource.kt
+++ app/src/main/java/com/eapvlab/movieapp/data/remote/MovieDataSource.kt
@@ -1,5 +1,19 @@
 package com.eapvlab.movieapp.data.remote
 
+import com.eapvlab.movieapp.data.model.MovieList
+
 class MovieDataSource {
+
+    fun getUpcomingMovies(): MovieList {
+        return MovieList()
+    }
+    
+    fun getTopRatedMovies(): MovieList {
+        return MovieList()
+    }
+
+    fun getPopularMovies(): MovieList {
+        return MovieList()
+    }
 
 }
