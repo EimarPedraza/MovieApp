d344ce924d3bdc1184421b6347e4f847c582b86d
Eimar Pedraza <eimar2alveiro@gmail.com>
2025-04-18 22:21:41 -0500

    IMPLEMENTACION - Se habilita viewBinding
    - Se agrega en el app build.gradle viewBinding en true dentro de buildFeatures.
    - Se inicializan las vistas en MovieFragment y MovieDetailFragment.
    
    Nota: Este permite ahorrarse la busqueda de los items, creas una instacia de la vista, y de alli puedes llamar o modificar sus elementos.

--- app/src/main/java/com/eapvlab/movieapp/MovieDetailFragment.kt
+++ app/src/main/java/com/eapvlab/movieapp/MovieDetailFragment.kt
@@ -3,11 +3,15 @@ package com.eapvlab.movieapp
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import com.eapvlab.movieapp.databinding.FragmentMovieDetailBinding
 
 class MovieDetailFragment : Fragment(R.layout.fragment_movie_detail) {
 
+    private lateinit var binding: FragmentMovieDetailBinding
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentMovieDetailBinding.bind(view)
     }
 
 }
